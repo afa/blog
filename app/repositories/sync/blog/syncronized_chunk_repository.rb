9955d5354ba8a4557b7633b292ecb1e2a4a841c3
Afa <afa.alone@gmail.com>
2024-05-12 03:31:15 +0300

    done repository #all for sync blog events

--- app/repositories/sync/blog/syncronized_chunk_repository.rb
+++ app/repositories/sync/blog/syncronized_chunk_repository.rb
@@ -13,11 +13,27 @@ module Sync
         return cached_data if cache?
 
         data = yield Sync::Protocol::Blog::Getevents.call(last_sync:, source:)
-        refresh_cache(data)
-        cached_data
+        events = yield merge_props(data['events'], data['props'])
+        refresh_cache(events)
+        # cached_data
       end
 
       def find(key); end
+
+      private
+
+      def merge_props(events, props)
+        Try {
+          idx = props.group_by { |hsh| hsh['itemid'] }
+          events.each_with_object([]) do |event, obj|
+            obj << event
+            if idx.key?(event['itemid'])
+              obj.last.merge!({ 'props' => idx[event['itemid']].map { |item| item.slice('name', 'value') } })
+            end
+          end
+        }
+          .to_result
+      end
     end
   end
 end
