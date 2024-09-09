0a5291779c95da86d76f4e6b9d2c4a3ae9282f05
afa <afa.alone@gmail.com>
2024-09-09 12:37:13 +0300

    fast links rewrite (#29)
    * start change links
    
    * fast link refactor done

---
+++ app/interactors/link/codes.rb
@@ -0,0 +1,8 @@
+module Link
+  module Codes
+    # without 1Ili, OQ0, S5
+    FIRST_LETTER =
+      %w[A B C D E F G H J K L M N P R T U V W X Y Z a b c d e f h j k m n o p r s t u v w x y z].freeze
+    LETTERS = FIRST_LETTER + %w[2 3 4 6 7 8 9]
+  end
+end
