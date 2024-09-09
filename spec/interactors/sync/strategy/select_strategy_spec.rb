2236ee72e2da26374615f8915f66e1262b3b6554
Afa <afa.alone@gmail.com>
2024-09-09 13:34:12 +0300

    spec for select strategy

--- spec/interactors/sync/strategy/select_strategy_spec.rb
+++ spec/interactors/sync/strategy/select_strategy_spec.rb
@@ -1,3 +1,40 @@
 RSpec.describe Sync::Strategy::SelectStrategy do
-  pending :todo
+  let(:interactor_call) { described_class.call(source:) }
+  let(:value) { interactor_call.value!}
+
+  context 'with blank strategy' do
+    let(:source) { Sync::Source.new name: 'a', strategy: '' }
+
+    it 'return success' do
+      expect(interactor_call).to be_success
+    end
+
+    it 'return zero strategy' do
+      expect(value).to eq(Sync::Strategy::ZeroStrategy)
+    end
+  end
+
+  context 'with nil strategy' do
+    let(:source) { Sync::Source.new name: 'a', strategy: nil }
+
+    it 'return success' do
+      expect(interactor_call).to be_success
+    end
+
+    it 'return zero strategy' do
+      expect(value).to eq(Sync::Strategy::ZeroStrategy)
+    end
+  end
+
+  context 'with running total' do
+    let(:source) { Sync::Source.new name: 'a', strategy: 'running_total' }
+
+    it 'return success' do
+      expect(interactor_call).to be_success
+    end
+
+    it 'return zero strategy' do
+      expect(value).to eq(Sync::Strategy::RunningTotal)
+    end
+  end
 end
